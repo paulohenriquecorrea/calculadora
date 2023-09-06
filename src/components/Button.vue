1522f5d6c9f644b09e71e8e9d496b485a05484a1
unknown <ph.sistemas.ti@gmail.com>
2023-09-06 10:39:37 -0400

    First commit

---
+++ src/components/Button.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+    class="button"
+    @click="$emit('onClick', label)"
+    :class="{ double, triple, operation }"
+  >
+    {{ label }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonDefault",
+  props: {
+    label: {},
+    operation: { type: Boolean },
+    double: { type: Boolean },
+    triple: { type: Boolean },
+  },
+};
+</script>
+
+<style scope>
+:root {
+  --bg-button: #f0f0f0;
+  --border-button: solid 1px #888;
+}
+.button {
+  font-size: 1.4em;
+  background-color: var(--bg-button);
+  border: none;
+  border-right: var(--border-button);
+  border-bottom: var(--border-button);
+  outline: none;
+  color: black;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.button:active {
+  background-color: #ccc;
+}
+
+.button.double {
+  grid-column: span 2;
+}
+
+.button.triple {
+  grid-column: span 3;
+}
+
+.button.operation {
+  background-color: #fa8231;
+  color: #fff;
+}
+
+.button.operation:active {
+  background-color: #fa8231cc;
+}
+</style>
---
+++ src/components/Display.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="display">{{ value }}</div>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  name: "DisplayDefault",
+};
+</script>
+
+<style scope>
+.display {
+  grid-column: span 4;
+  background-color: #0004;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 20px;
+  font-size: 2.1em;
+  overflow: hidden;
+}
+</style>
